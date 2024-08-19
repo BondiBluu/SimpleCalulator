1f107cf672f54968da7560848c8dd5be765df78f
Bami O. <bamideleogundeji949@gmail.com>
2024-08-19 10:18:28 -0400

    Add project files.

---
+++ Calulator/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Calc Program");
+
+            Calc();
+
+            
+
+            Console.ReadKey();
+        }
+
+        public static void Calc()
+        {
+            string continueOperation;
+            int num1;
+            int num2;
+
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter operation symbol: ");
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine("Result: " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("Result: " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("Result: " + (num1 * num2));
+                    break;
+                case "/":
+                    Console.WriteLine("Result: " + (num1 / num2));
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+
+            Console.WriteLine("Keep going? y or n: ");
+            continueOperation = Console.ReadLine();
+
+            if (continueOperation == "y")
+            {
+                Calc();
+            }
+            else
+            {
+                Console.WriteLine("Terminating");
+            }
+        }
+    }
+}
